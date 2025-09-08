ec16617bc9b5afce3bc6a3c42b61461a223bcbe1
SavvySantos <ssantos@byupathway.edu>
2025-09-08 15:23:13 -0600

    fixed my JS so it would store my scriptures as a list instead of replacing them as new ones are typed

--- week02/scripts/getscriptures.js
+++ week02/scripts/getscriptures.js
@@ -1,22 +1,26 @@
 const button = document.querySelector('button');
 const input = document.querySelector('#favchap');
 const list = document.querySelector('ul'); // you need to fill in the blank to reference the HTML elelement that is unordered list element.
-const li = document.createElement('li');
-const deleteButton = document.createElement('button');
 
 button.addEventListener('click', function() {
     // Code to execute when the button is clicked
     if(input.value.trim()!=='') {
+        const li = document.createElement('li');
+        const deleteButton = document.createElement('button');
+
         li.textContent = input.value;
         deleteButton.textContent = '❌';
+
         li.append(deleteButton);
         list.append(li);
-    }
-    deleteButton.addEventListener('click', function() {
-        list.removeChild(li);
-        input.focus();
-        input.value = ''
-    }) 
+
+        deleteButton.addEventListener('click', function() {
+            list.removeChild(li);
+            input.focus();
+            input.value = ''
+    })
+
+    input.value = "";
+    input.focus();
+}
 });
-
-
