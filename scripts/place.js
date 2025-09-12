3304c145252ee4461ae429f2a05eccaaecc6f843
SavvySantos <ssantos@byupathway.edu>
2025-09-12 01:23:22 -0600

    changes to js

--- scripts/place.js
+++ scripts/place.js
@@ -6,26 +6,38 @@ document.getElementById("lastmodified").innerHTML = date;
 
 const apiKey = "c93a13b5523e51a7d6d34fc3797c35d7";
 const city = "Edingurgh";
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=edinburgh&appid=c93a13b5523e51a7d6d34fc3797c35d7&units=imperial';
+
     async function getWeather()
     {
-        const url = 'https://api.openweathermap.org/data/2.5/weather?q=edinburgh&appid=c93a13b5523e51a7d6d34fc3797c35d7&units=imperial';
 
         const response = await fetch(url);
         const data = await response.json();
 
-        let temperature = data.main.temp;
-        let condition = data.weather[0].description;
-        let windSpeed = data.wind.speed;
+        const temperature = data.main.temp;
+        const condition = data.weather[0].description;
+        const windSpeed = data.wind.speed;
 
         document.getElementById("temperature").textContent = data.main.temp.toFixed(1);
         document.getElementById("condition").textContent = data.weather[0].description;
         document.getElementById("wind").textContent = data.wind.speed;
+        
+        const windChill = calculateWindChill(temperature, windSpeed)
+        document.getElementById("wind-chill").textContent = windChill.toFixed(2);
 
-
-        let windChill = (temperature <= 50 && windSpeed > 3)
-            ? (35.74 + 0.6215 * temperature - 35.75 * Math.pow(windSpeed, 0.16) + 0.4275 * temperature * Math.pow(windSpeed, 0.16)).toFixed(1)
-            : "N/A";            
-        document.getElementById("wind-chill").textContent = windChill;
+        function calculateWindChill(temperature, windSpeed) {
+            if (temperature > 50 || windSpeed <= 3) {
+                const windChill = 35.74 + 0.6215 * temperature - 35.75 * Math.pow(windSpeed, 0.16) + 0.4275 * temperature * Math.pow(windSpeed, 0.16);
+                return windChill;
+        }
+        else {
+            return temperature;
+        }
+    }
+        //let windChill = (temperature <= 50 && windSpeed > 3)
+           // ? (35.74 + 0.6215 * temperature - 35.75 * Math.pow(windSpeed, 0.16) + 0.4275 * temperature * Math.pow(windSpeed, 0.16)).toFixed(1)
+            //: "N/A";            
+        //document.getElementById("wind-chill").textContent = windChill;
         }
         getWeather("Edinburgh");
         setInterval(getWeather("Edinburgh"), 600000);
