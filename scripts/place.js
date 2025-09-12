2410e55804c7bba4e31e09336182f531eb3033cf
SavvySantos <ssantos@byupathway.edu>
2025-09-11 20:04:02 -0600

    the start of my country page assignment

---
+++ scripts/place.js
@@ -0,0 +1,5 @@
+var date = new Date ();
+var year = date.getFullYear();
+document.getElementById("currentyear").innerHTML = year;
+var date = document.lastModified;
+document.getElementById("lastmodified").innerHTML = date;
