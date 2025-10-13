8d7a48c0ba140ac99249c5b8de16fa99a7316077
SavvySantos <ssantos@byupathway.edu>
2025-10-12 22:54:36 -0600

    contact us page

--- week06/scripts/santos.js
+++ week06/scripts/santos.js
@@ -1,5 +1,6 @@
 const currentYear = new Date ().getFullYear();
 document.getElementById("currentyear").textContent = currentYear;
+
 const lastModified = document.lastModified;
 document.getElementById("lastmodified").textContent = lastModified;
 
@@ -7,6 +8,28 @@ document.getElementById("lastmodified").textContent = lastModified;
 document.addEventListener("DOMContentLoaded", () => {
     const menuButton = document.getElementById("menu");
     const navList = document.querySelector(".navigation");
+
+    let currentPage = window.location.pathname.split("/").pop();
+    if(currentPage === "" || currentPage ==="/")
+    {
+        currentPage = "index.html";
+    }
+
+    let navLinks = document.querySelectorAll(".main-nav a");
+
+    navLinks.forEach(link => {
+        let linkPage = link.getAttribute("href").split("/").pop();
+        if(linkPage === "" || linkPage === "/") {
+            linkPage = "index.html";
+        }
+
+        if (linkPage === currentPage) {
+            link.classList.add("using");
+        }
+        else {
+            link.classList.remove("using")
+        }
+    });
 
     menuButton.addEventListener("click", () =>{
         menuButton.classList.toggle("open");
